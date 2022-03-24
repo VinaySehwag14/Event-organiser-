a3024220143afe2713f4a6eff06209def88446b0
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-03-24 12:28:22 +0530

    using dynamic data and setting not found page

--- pages/[pid].jsx
+++ pages/[pid].jsx
@@ -1,25 +1,44 @@
 import path from "path";
 import fs from "fs/promises";
 
-const productDetailPage = ({ loadedProduct }) => {
+import { Fragment } from "react";
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <>
+    <Fragment>
       <h1>{loadedProduct.title}</h1>
       <p>{loadedProduct.description}</p>
-    </>
+    </Fragment>
   );
-};
+}
+
+async function getData() {
+  const filePath = path.join(process.cwd(), "dummyBackend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
 
 export async function getStaticProps(context) {
   const { params } = context;
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "dummyBackend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       loadedProduct: product,
@@ -28,14 +47,15 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
-    fallback: false,
+    paths: pathsWithParams,
+    fallback: true,
   };
 }
 
-export default productDetailPage;
+export default ProductDetailPage;
