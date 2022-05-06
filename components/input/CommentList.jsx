f32f8367d772d19f151166d79210ff3b23692039
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-05-06 10:44:08 +0530

    added components with basic styling

---
+++ components/input/CommentList.jsx
@@ -0,0 +1,20 @@
+import styles from "./CommentList.module.css";
+
+function CommentList(props) {
+  const { items } = props;
+
+  return (
+    <ul className={styles.comments}>
+      {items.map((item) => (
+        <li key={item._id}>
+          <p>{item.text}</p>
+          <div>
+            By <address>{item.name}</address>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default CommentList;
