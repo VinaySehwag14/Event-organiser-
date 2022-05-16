6dab62dd5bb1cfa6b256c2896b177f8c9310b7fe
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-05-16 12:47:05 +0530

    added ntification logic and removing logic

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,6 @@ function MyApp({ Component, pageProps }) {
           />
         </Head>
         <Component {...pageProps} />
-        <Notification title="TEST" message="this is a test" />
       </Layout>
     </NotificationContextProvider>
   );
