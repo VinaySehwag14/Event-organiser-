f4d6537a119ba2439f502613362a79687f21e798
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-06-24 18:52:54 +0530

    useSession hook is used to signIn and signOut and also protecting routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
+//* it is necessary for using useSession to wrap component with session Provider
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
 
